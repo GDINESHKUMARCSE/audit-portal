a57b6497629648739d9af6944f3da83438107fee
gdineshkumarcse <gdineshkumarcse@gmail.com>
2022-05-17 21:01:39 +0530

    first commit

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../model/user";
+import {UserRecord} from "../model/user_record";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(user: User) {
+    return this.http.post<UserRecord>('http://localhost:8083/login', user);
+  }
+}
